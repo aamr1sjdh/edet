038c3efebf5ff490f2f4c02df96fda748f65d854
Shbjf <136927197+Shbjf@users.noreply.github.com>
2024-06-06 11:54:51 +0300

    Update cmdadd.js

--- plugins/cmdadd.js
+++ plugins/cmdadd.js
@@ -1,13 +1,20 @@
+
+
 const handler = async (m, {conn, text, usedPrefix, command}) => {
+  const datas = global
+  const idioma = datas.db.data.users[m.sender].language
+  const _translate = JSON.parse(fs.readFileSync(`./language/${idioma}.json`))
+  const tradutor = _translate.plugins.cmd_add
+
   global.db.data.sticker = global.db.data.sticker || {};
-  if (!m.quoted) throw '*☁️ Faltó Un Sticker E Imagen*';
-  if (!m.quoted.fileSha256) throw '🛑 *Solo Sticker E Inagenes*';
-  if (!text) throw `*☁️ Faltó Un Stiker O Imagen*`;
+  if (!m.quoted) throw `*${tradutor.texto1}*`;
+  if (!m.quoted.fileSha256) throw `*${tradutor.texto2}*`;
+  if (!text) throw `*${tradutor.texto3[0]}*\n*—◉ ${usedPrefix + command} ${tradutor.texto3[1]}*\n\n*${tradutor.texto3[2]}*\n*—◉ ${usedPrefix + command} <#menu> ${tradutor.texto3[3]}*`;
   const sticker = global.db.data.sticker;
   const hash = m.quoted.fileSha256.toString('base64');
-  if (sticker[hash] && sticker[hash].locked) throw '*🛑 Solo El Creador Puede Usar Esta Función*';
+  if (sticker[hash] && sticker[hash].locked) throw `*${tradutor.texto4}*`;
   sticker[hash] = {text, mentionedJid: m.mentionedJid, creator: m.sender, at: + new Date, locked: false};
-  m.reply(`☁️ *Agregado Con Exito*`);
+  m.reply(`*${tradutor.texto5}*`);
 };
 handler.command = ['setcmd', 'addcmd', 'cmdadd', 'cmdset'];
 handler.rowner = true;
